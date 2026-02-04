1890f4541d67a15e7829e13e1407a32ca0662911
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2026-02-04 06:01:42 +0100

    feat: implement Header and Hero components; update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import "./App.css";
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <>
-      <header className="max-w-90 flex items-center justify-center my-5">
-        <a className="flex flex-col gap-2 items-center justify-center" href="#">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="36"
-            height="36"
-            fill="blue"
-            className="bi bi-fuel-pump"
-            viewBox="0 0 16 16"
-          >
-            <path d="M3 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-.5.5h-5a.5.5 0 0 1-.5-.5z" />
-            <path d="M1 2a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v8a2 2 0 0 1 2 2v.5a.5.5 0 0 0 1 0V8h-.5a.5.5 0 0 1-.5-.5V4.375a.5.5 0 0 1 .5-.5h1.495c-.011-.476-.053-.894-.201-1.222a.97.97 0 0 0-.394-.458c-.184-.11-.464-.195-.9-.195a.5.5 0 0 1 0-1q.846-.002 1.412.336c.383.228.634.551.794.907.295.655.294 1.465.294 2.081v3.175a.5.5 0 0 1-.5.501H15v4.5a1.5 1.5 0 0 1-3 0V12a1 1 0 0 0-1-1v4h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1zm9 0a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v13h8z" />
-          </svg>
-          <h1 className="text-3xl font-bold">FuelFinder</h1>
-        </a>
-      </header>
+      <Header />
+    
     </>
   );
 }
